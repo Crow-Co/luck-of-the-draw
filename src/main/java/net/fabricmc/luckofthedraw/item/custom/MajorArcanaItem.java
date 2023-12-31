92f42d0c412fa715cfecec7045c3921850a0cb6e
Zeraphynx <gamechannel6@hotmail.com>
2023-12-31 07:51:02 +0400

    Base MA Item

---
+++ src/main/java/net/fabricmc/luckofthedraw/item/custom/MajorArcanaItem.java
@@ -0,0 +1,9 @@
+package net.fabricmc.luckofthedraw.item.custom;
+
+import net.minecraft.item.Item;
+
+public class MajorArcanaItem extends Item {
+    public MajorArcanaItem(Settings settings) {
+        super(settings.maxCount(1));
+    }
+}
