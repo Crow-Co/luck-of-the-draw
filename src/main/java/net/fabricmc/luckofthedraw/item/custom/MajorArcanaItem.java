98ad5b73d84aa674553b7d3840fba6c91593e371
Chox <mohdsamir048@gmail.com>
2023-12-31 08:05:34 +0400

    Change the durability bar to cyan and add comments

--- src/main/java/net/fabricmc/luckofthedraw/item/custom/MajorArcanaItem.java
+++ src/main/java/net/fabricmc/luckofthedraw/item/custom/MajorArcanaItem.java
@@ -1,9 +1,18 @@
 package net.fabricmc.luckofthedraw.item.custom;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 
 public class MajorArcanaItem extends Item {
+    // Base settings
     public MajorArcanaItem(Settings settings) {
         super(settings.maxCount(1));
+    }
+
+    // Cooldown bar color
+    @Override
+    public int getItemBarColor(ItemStack stack) {
+        return MathHelper.hsvToRgb(0.5f, 1.0F, 1.0F);
     }
 }
