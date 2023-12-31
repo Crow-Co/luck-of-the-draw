c34359340990681378bc69cf6ba1c4e26f9e0656
Chox <mohdsamir048@gmail.com>
2024-01-01 03:54:51 +0400

    Made the cooldown for items functional, hopefully

--- src/main/java/net/fabricmc/luckofthedraw/item/custom/MajorArcanaItem.java
+++ src/main/java/net/fabricmc/luckofthedraw/item/custom/MajorArcanaItem.java
@@ -1,8 +1,10 @@
 package net.fabricmc.luckofthedraw.item.custom;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 
 public abstract class MajorArcanaItem extends Item {
     // Base settings
@@ -14,7 +16,7 @@ public abstract class MajorArcanaItem extends Item {
            if (cooldownTime > 0) {
                settings.maxDamage(cooldownTime);
            }
-    };
+    }
 
     // Cooldown bar color
     @Override
@@ -22,4 +24,13 @@ public abstract class MajorArcanaItem extends Item {
         return MathHelper.hsvToRgb(0.5f, 1.0F, 1.0F);
     }
 
+    // The code for the Cooldown. NOTE: 1 tick = 1/20th of a second, this is running at 20 ticks per second
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (stack.getItem() instanceof MajorArcanaItem) {
+            if (stack.getDamage() < stack.getMaxDamage()) {
+                stack.setDamage(stack.getDamage() - 1);
+            }
+        }
+    }
 }
