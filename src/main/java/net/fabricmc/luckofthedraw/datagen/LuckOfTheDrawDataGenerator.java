8d1ecdeac55154aeee0c9a525d9d54ed3657d804
Chox <mohdsamir048@gmail.com>
2024-01-03 02:37:13 +0400

    Changes to configuration files to allow data generation for future advancement integration

---
+++ src/main/java/net/fabricmc/luckofthedraw/datagen/LuckOfTheDrawDataGenerator.java
@@ -0,0 +1,15 @@
+package net.fabricmc.luckofthedraw.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LuckOfTheDrawDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+
+        // Adding a provider example:
+        //
+        // pack.addProvider(AdvancementsProvider::new);
+    }
+}
