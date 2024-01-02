52de3b5d36ac65cf98bc9c66e2a7d72a69521098
Chox <mohdsamir048@gmail.com>
2024-01-03 03:06:46 +0400

    Updating the data generator to be ready for advancements

--- src/main/java/net/fabricmc/luckofthedraw/datagen/LuckOfTheDrawDataGenerator.java
+++ src/main/java/net/fabricmc/luckofthedraw/datagen/LuckOfTheDrawDataGenerator.java
@@ -2,14 +2,29 @@ package net.fabricmc.luckofthedraw.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
+import net.minecraft.advancement.*;
+import java.util.function.Consumer;
 
 public class LuckOfTheDrawDataGenerator implements DataGeneratorEntrypoint {
+    // Initialization for the data generator
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
 
-        // Adding a provider example:
-        //
-        // pack.addProvider(AdvancementsProvider::new);
+        // Adding the providers to the initialization
+        pack.addProvider(AdvancementsProvider::new);
+    }
+
+    static class AdvancementsProvider extends FabricAdvancementProvider {
+        protected AdvancementsProvider(FabricDataOutput dataGenerator) {
+            super(dataGenerator);
+        }
+
+        @Override
+        public void generateAdvancement(Consumer<Advancement> consumer) {
+            // Future custom advancements will be added here
+        }
     }
 }
