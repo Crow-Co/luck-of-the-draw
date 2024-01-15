88968583505eec2616fe1e29cb2ead4a5c469d9a
Miera <Gamechannel6@hotmail.com>
2024-01-15 08:39:48 +0400

    Added a default on use to MAI

--- src/main/java/net/luckofthedraw/item/custom/MajorArcanaItem.java
+++ src/main/java/net/luckofthedraw/item/custom/MajorArcanaItem.java
@@ -1,8 +1,11 @@
 package net.luckofthedraw.item.custom;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
@@ -32,5 +35,10 @@ public abstract class MajorArcanaItem extends Item {
                 stack.setDamage(stack.getDamage() - 1);
             }
         }
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return super.use(world, user, hand);
     }
 }
