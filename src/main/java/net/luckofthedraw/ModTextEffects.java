e1a4a81d3c6ff31231061e08e5051441d81bb247
Chox Raven <mohdsamir048@gmail.com>
2024-04-22 22:58:08 +0400

    Added a new effect; Hologram

--- src/main/java/net/luckofthedraw/ModTextEffects.java
+++ src/main/java/net/luckofthedraw/ModTextEffects.java
@@ -1,10 +1,12 @@
 package net.luckofthedraw;
 
+import net.luckofthedraw.textEffects.HologramEffect;
 import net.luckofthedraw.textEffects.SmallWaveEffect;
 import snownee.textanimator.effect.EffectFactory;
 
 public class ModTextEffects {
     public static void registerModTextEffects() {
         EffectFactory.register("small-wave", SmallWaveEffect::new);
+        EffectFactory.register("hologram", HologramEffect::new);
     }
 }
